e21f43984d19b664dc246abc9db9a6b40854956b
dhairya-2004 <dhairya2004first@gmail.com>
2024-07-30 18:08:07 +0530

    EmptyString case

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,8 @@
 public class StringCalculator {
+    public int add(String numbers) {
+        if (numbers.isEmpty()) {  //if string is empty
+            return 0;
+        }
+        return -1;
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
+
+    @Test
+    public void testAddEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
 }
